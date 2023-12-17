09cfd4e74c125bc353d3a312e4a32addcdfb5c0b
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-12-17 18:36:21 +0700

    Init the project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1 class="title">Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'App'
+  });
+
+</script>
+
+<style lang="scss">
+  .container{
+    width: 100%;
+    max-width: 1430px;
+    padding: 0 15px;
+    margin: 0 auto;
+  }
+</style>
