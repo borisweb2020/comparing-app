1c5d0f90ff61f03ff00987c8291e94b593d7bc49
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-12-18 20:46:36 +0700

    Complete the table section markup

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,26 @@
   <div class="container">
     <intro-app></intro-app>
   </div>
+  <table-app></table-app>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import HeaderApp from '@/components/HeaderApp.vue';
   import IntroApp from './components/IntroApp.vue';
+  import TableApp from './components/TableApp.vue';
+
+
 
   export default defineComponent({
     name: 'App',
     components: {
       HeaderApp,
-      IntroApp
+      IntroApp,
+      TableApp
     },
+
+
   });
 
 </script>
---
+++ src/components/TableApp.vue
@@ -0,0 +1,68 @@
+<template>
+	<section class="table">
+		<div class="container">
+			<div class="table__inner">
+				<div class="table__header">
+					<div class="table__header-item">Производитель</div>
+					<div class="table__header-item">Год релиза</div>
+					<div class="table__header-item">Диагональ экрана (дюйм)</div>
+					<div class="table__header-item">страна-производитель</div>
+					<div class="table__header-item">объем памяти</div>
+					<div class="table__header-item">частота обновления экрана</div>
+					<div class="table__header-item">nfc</div>
+					<div class="table__header-item">поддержка esim</div>
+					<div class="table__header-item">поддержка беспроводной зарядки</div>
+					<div class="table__header-item">стоимость</div>
+				</div>
+				<div class="table__smart">
+					<div class="table__smart-item">Apple</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+				</div>
+				<div class="table__smart">
+					<div class="table__smart-item">Xiaomi</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+				</div>
+				<div class="table__smart">
+					<div class="table__smart-item">Samsung</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+					<div class="table__smart-item">Lorem</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+
+	export default defineComponent ({
+		name: 'TableApp'
+	});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
