129d12ba3ce10a4138aef90747c6aa1968715d5b
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-12-19 18:31:16 +0700

    Perform the implementation of displaying items from an object array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <header-app></header-app>
   <div class="container">
-    <intro-app :smarts="smartphones"></intro-app>
+    <intro-app :smarts="smartphonesDisplay" @point="changeCount"></intro-app>
   </div>
   <table-app></table-app>
 </template>
@@ -21,21 +21,44 @@
 
     data(){
       return {
+        count: 3,
         smartphones: [
           {producer: 'Apple iPnone 12', release: '2020', diagonal: '6.1', state: 'Китай', memory: '128 Гб', refresh: '60 Гц', nfc: false, esim: true, charging: true, price: '81 990 Р', url: 'src/assets/images/apple-12.jpg'},
           {producer: 'Xiaomi Mi 11 Lite', release: '2021', diagonal: '6.55', state: 'Китай', memory: '128 Гб', refresh: '90 Гц', nfc: true, esim: true, charging: false, price: '27 490 Р', url: 'src/assets/images/xiaomi-11.jpg'},
           {producer: 'Samsung Galaxy A72', release: '2021', diagonal: '6.7', state: 'Вьетнам', memory: '128 Гб', refresh: '90 Гц', nfc: true, esim: false, charging: true, price: '32 890 Р', url: 'src/assets/images/samsung-72.jpg'},
+          {producer: 'Apple iPnone 50', release: '2020', diagonal: '6.1', state: 'Китай', memory: '128 Гб', refresh: '60 Гц', nfc: false, esim: true, charging: true, price: '81 990 Р', url: 'src/assets/images/apple-12.jpg'},
+          {producer: 'Xiaomi Mi 500 Lite', release: '2021', diagonal: '6.55', state: 'Китай', memory: '128 Гб', refresh: '90 Гц', nfc: true, esim: true, charging: false, price: '27 490 Р', url: 'src/assets/images/xiaomi-11.jpg'},
+          {producer: 'Samsung Galaxy 600', release: '2021', diagonal: '6.7', state: 'Вьетнам', memory: '128 Гб', refresh: '90 Гц', nfc: true, esim: false, charging: true, price: '32 890 Р', url: 'src/assets/images/samsung-72.jpg'},
+          {producer: 'Apple iPnone 888', release: '2020', diagonal: '6.1', state: 'Китай', memory: '128 Гб', refresh: '60 Гц', nfc: false, esim: true, charging: true, price: '81 990 Р', url: 'src/assets/images/apple-12.jpg'},
+          {producer: 'Xiaomi Mi 444', release: '2021', diagonal: '6.55', state: 'Китай', memory: '128 Гб', refresh: '90 Гц', nfc: true, esim: true, charging: false, price: '27 490 Р', url: 'src/assets/images/xiaomi-11.jpg'},
         ],
+        smartphonesDisplay: [],
       }
     },
 
     mounted(){
+      this.displaySmarts(this.count);
       this.displayLog();
     },
 
     methods: {
       displayLog(){
-        console.log(this.smartphones.length);
+        console.log(this.count);
+        console.log(this.smartphones);
+      },
+
+      changeCount(value){
+        this.count = value;
+        this.smartphonesDisplay = [];
+        this.smartphonesDisplay = [].concat(this.smartphones);
+        this.smartphonesDisplay.length = this.count;
+        return this.smartphonesDisplay;
+      },
+
+      displaySmarts(count){
+        this.smartphonesDisplay = [].concat(this.smartphones);
+        this.smartphonesDisplay.length = count;
+        return this.smartphonesDisplay;
       }
     }
 
--- src/components/IntroApp.vue
+++ src/components/IntroApp.vue
@@ -6,7 +6,9 @@
 				<div class="intro__nav-title">Отобразить товары:</div>
 				<ul class="intro__nav-amount">
 					<li class="intro__nav-count"
-					v-for="i in 5" :key="i + 1">{{ i + 1 }}</li>
+					:class="{'active': count - 2 === index}"
+					v-for="(i, index) in 5" :key="i + 1"
+					@click="pointCount(index)">{{ i + 1 }}</li>
 				</ul>
 			</nav>
 		</div>
@@ -28,26 +30,6 @@
 					</div>
 					<div class="intro__item-name">{{ item.producer }}</div>
 				</div>
-
-				<!-- <div class="intro__item">
-					<div class="intro__item-wrapper">
-						<img class="intro__item-img" src="../assets/images/xiaomi-11.jpg" alt="phone">
-						<button class="intro__item-btn">
-							<img src="../assets/images/arrow-down.svg" alt="arrow">
-						</button>
-					</div>
-					<div class="intro__item-name">Xiaomi Mi 11 Lite</div>
-				</div>
-				<div class="intro__item">
-					<div class="intro__item-wrapper">
-						<img class="intro__item-img" src="../assets/images/samsung-72.jpg" alt="phone">
-						<button class="intro__item-btn">
-							<img src="../assets/images/arrow-down.svg" alt="arrow">
-						</button>
-					</div>
-					<div class="intro__item-name">Samsung Galaxy A72</div>
-				</div> -->
-
 			</div>
 		</div>
 	</section>
@@ -56,6 +38,19 @@
 <script>
 
 	export default {
+		data(){
+			return{
+				count: 3,
+			}
+		},
+
+		methods: {
+			pointCount(index){
+				this.count = (index + 2);
+				this.$emit('point', this.count);
+			}
+		},
+
 		props: {
 			smarts: {
 				type: Array,
